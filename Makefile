60a0ce3772613cd7639af163eeb4b92c3f953fd0
Mark Chernyshev <mchernys@ucsd.edu>
2021-06-29 11:27:23 -0700

    initial commit

---
+++ Makefile
@@ -0,0 +1,34 @@
+PLOT_SRC=create_networks_and_plot.py
+PLOT_EXE=python3
+PROTEIN_LINKS_TXT=9606.protein.links.v11.0.txt.gz
+PROTEIN_DOMAINS_TXT=proteins_w_domains.txt
+BOXPLOT=protein_domains_vs_string_degree.png
+
+.PHONY : all
+all : $(BOXPLOT)
+
+$(BOXPLOT) : $(PLOT_SRC) $(PROTEIN_LINKS_TXT) $(PROTEIN_DOMAINS_TXT)
+	$(PLOT_EXE) $^
+
+.PHONY : PROTEIN_LINKS_TXT
+$(PROTEIN_LINKS_TXT) :
+	wget https://stringdb-static.org/download/protein.links.v11.0/9606.protein.links.v11.0.txt.gz
+
+.PHONY : PROTEIN_DOMAINS_TXT
+$(PROTEIN_DOMAINS_TXT) :
+	wget https://stockholmuniversity.box.com/shared/static/n8l0l1b3tg32wrzg2ensg8dnt7oua8ex
+	mv n8l0l1b3tg32wrzg2ensg8dnt7oua8ex $(PROTEIN_DOMAINS_TXT)
+
+.PHONY : clean
+clean :
+	rm -rf $(BOXPLOT)
+	rm -rf $(PROTEIN_LINKS_TXT)
+	rm -rf $(PROTEIN_DOMAINS_TXT)
+
+.PHONY : variables
+variables:
+	@echo BOXPLOT: $(BOXPLOT)
+	@echo PLOT_SRC: $(PLOT_SRC)
+	@echo PLOT_EXE: $(PLOT_EXE)
+	@echo PROTEIN_LINKS_TXT: $(PROTEIN_LINKS_TXT)
+	@echo PROTEIN_DOMAINS_TXT: $(PROTEIN_DOMAINS_TXT)
---
+++ create_networks_and_plot.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
+import seaborn as sns
+import networkx as nx
+import sys
+
+#protein_links_file="9606.protein.links.v11.0.txt.gz"
+#protein_domains_file="proteins_w_domains.txt"
+#boxplot_file="protein_domains_vs_string_degree.png"
+protein_links_file = sys.argv[1]
+protein_domains_file = sys.argv[2]
+boxplot_file = "protein_domains_vs_string_degree.png"
+
+network_data_frame = pd.read_csv(protein_links_file, sep=' ')
+network = nx.from_pandas_edgelist(network_data_frame, source = "protein1", target = "protein2", edge_attr= "combined_score")
+original_num_edges = network.number_of_edges()
+
+sig_combined_score = 500
+selected_edges = [(u,v) for u,v,e in network.edges(data=True) if e['combined_score'] >= sig_combined_score]
+network = network.edge_subgraph(selected_edges)
+new_num_edges = network.number_of_edges()
+
+sig_node_degree = 100
+greater_degree_nodes = [n for n in network.nodes() if network.degree[n] > sig_node_degree]
+lesser_degree_nodes = [n for n in network.nodes() if network.degree[n] <= sig_node_degree]
+greater_degree_network = network.subgraph(greater_degree_nodes)
+lesser_degree_network = network.subgraph(lesser_degree_nodes)
+
+prot_domains_per_id_df = pd.read_csv(protein_domains_file, sep = '\t')
+prot_domains_per_id_df = prot_domains_per_id_df.loc[~prot_domains_per_id_df["Pfam ID"].isnull(),:]
+
+ctr_dict = dict()
+for prot_stable_id, df in prot_domains_per_id_df.groupby(by = "Protein stable ID"):
+    ctr_dict[prot_stable_id] = len(df['Pfam ID'].unique())
+greater_degree_domains = [ctr_dict[n.split(".")[1]] for n in greater_degree_network.nodes() if n.split(".")[1] in ctr_dict]
+lesser_degree_domains = [ctr_dict[n.split(".")[1]] for n in lesser_degree_network.nodes() if n.split(".")[1] in ctr_dict]
+
+df1 = pd.DataFrame()
+df1['domains'] = greater_degree_domains
+df1['degrees'] = 'greater'
+df2 = pd.DataFrame()
+df2['domains'] = lesser_degree_domains
+df2['degrees'] = 'lesser'
+df = pd.concat([df1, df2]).reset_index(drop = True)
+
+p = sns.catplot(x = 'degrees', y='domains',data =df, kind = 'box')
+p.savefig(boxplot_file)
